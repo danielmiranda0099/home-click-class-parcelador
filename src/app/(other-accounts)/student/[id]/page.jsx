1c502dfd9d032818178ba65e64702ad8205ebf79
danielmiranda0099 <danielmesa877@gmail.com>
2024-09-19 16:01:34 -0500

    [FIX]

--- src/app/(other-accounts)/student/[id]/page.jsx
+++ src/app/(other-accounts)/student/[id]/page.jsx
@@ -5,11 +5,13 @@ import {
   CardUpcomingLesson,
   FormConfirmClass,
 } from "@/components";
+import { PopupDetailLesson } from "@/components/popupDetailLesson";
 
 export default function StudentsPage() {
   return (
     <>
       <FormConfirmClass />
+      <PopupDetailLesson rol={"student"} />
       <section className="px-10 py-3 flex flex-row justify-start gap-4">
         <CardOverView />
         <CardStatusLegendLesson rol="student" />
--- src/app/(other-accounts)/teacher/[id]/page.jsx
+++ src/app/(other-accounts)/teacher/[id]/page.jsx
@@ -4,10 +4,12 @@ import {
   Card,
   CardStatusLegendLesson,
 } from "@/components";
+import { PopupDetailLesson } from "@/components/popupDetailLesson";
 
 export default async function TeacherPage() {
   return (
     <>
+      <PopupDetailLesson rol={"teacher"} />
       <section className="px-10 py-3 flex flex-row justify-start gap-4">
         <CardOverView />
         <CardStatusLegendLesson rol="teacher" />
