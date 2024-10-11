4854e5c2d51f7b05195f1d877bdf2f3b1a3619a6
danielmiranda0099 <danielmesa877@gmail.com>
2024-10-11 10:52:15 -0500

    [MINOR UI] Min height Layout Admin

--- src/app/(admin)/layout.jsx
+++ src/app/(admin)/layout.jsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <>
       <Header />
-      <div className="flex">
+      <div className="flex min-h-screen">
         {/* Sidebar */}
 
         <SideBarMenu />
