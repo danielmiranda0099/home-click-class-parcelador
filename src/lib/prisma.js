979c5d96cc827930887032d8db5e84defc3ef843
danielmiranda0099 <danielmesa877@gmail.com>
2024-11-04 18:27:58 -0500

    [FIX]

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
