0baa754de1db68f97dcc402746e1f5b9dca5dcfa
danielmiranda0099 <danielmesa877@gmail.com>
2024-11-04 21:21:32 -0500

    [ADD] Protectec Routes

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,21 @@
 import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
+import { NextResponse } from "next/server";
 
-export const { auth: middleware } = NextAuth(authConfig);
+const { auth: middleware } = NextAuth(authConfig);
+
+const public_routes = ["/"];
+
+export default middleware((req) => {
+  const { nextUrl, auth } = req;
+  const isLogged = !!auth?.user;
+
+  if (!public_routes.includes(nextUrl.pathname) && !isLogged) {
+    return NextResponse.redirect(new URL("/", nextUrl));
+  }
+
+  return NextResponse.next();
+});
 
 export const config = {
   matcher: [
