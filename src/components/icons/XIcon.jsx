98820989c709de8f5431ebbf7082324b1516d63a
danielmiranda0099 <danielmesa877@gmail.com>
2024-11-05 22:30:01 -0500

    [UP FIX]

--- src/components/icons/XIcon.jsx
+++ src/components/icons/XIcon.jsx
@@ -1,16 +1,16 @@
-export function XIcon(props) {
+export function XIcon({ size = 24, color = "currentColor", className = "" }) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={size}
+      height={size}
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke={color}
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      className={className}
     >
       <path d="M18 6 6 18" />
       <path d="m6 6 12 12" />
