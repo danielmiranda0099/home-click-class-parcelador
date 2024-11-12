49a2f210deba6b8fc62664139f3a0babf64e88e6
danielmiranda0099 <danielmesa877@gmail.com>
2024-11-12 18:17:06 -0500

    [ADD] Store User for Custom Hook User Session

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,4 +1,4 @@
-import { GetAllUsers } from "@/actions/CrudUser";
+import { GetAllUsers, getUserSession } from "@/actions/CrudUser";
 import { create } from "zustand";
 
 export const useUserStore = create((set) => ({
@@ -15,4 +15,12 @@ export const useUserStore = create((set) => ({
     set((state) => ({ users: [...state.users, new_user] })),
   user_selected: null,
   setuserSelected: (user) => set({ user_selected: user }),
+
+  user_session: null,
+  setUserSession: async () => {
+    const data_response = await getUserSession();
+    if (data_response.success) {
+      set({ user_session: data_response.data });
+    }
+  },
 }));
