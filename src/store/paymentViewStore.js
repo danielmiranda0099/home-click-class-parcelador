1e52a42d47d56d4ec6966484432a945b09b00684
danielmiranda0099 <danielmesa877@gmail.com>
2024-11-14 11:01:26 -0500

    [UP REFACT] Centralize Logic

--- src/store/paymentViewStore.js
+++ src/store/paymentViewStore.js
@@ -1,10 +1,17 @@
+import { formatPayments } from "@/utils/formatPayments";
+import { formattedLessonsForCalendar } from "@/utils/formattedLessonsForCalendar";
 import { create } from "zustand";
-//TODO: Change Name file uiStores quitar la "s"
+
 export const usePaymentViewStore = create((set) => ({
   start_date: "",
   setStartDate: (start_date) => set({ start_date }),
   end_date: "",
   setEndDate: (end_date) => set({ end_date }),
   payments: null,
-  setPayments: (payments) => set({ payments }),
+  setPayments: async (lesson_unpayments, user) => {
+    const unpaid_lesson_formated = formatPayments(lesson_unpayments, user);
+    const unpaid_lesson_formated_for_calendar =
+      await formattedLessonsForCalendar(unpaid_lesson_formated, "admin");
+    set({ payments: unpaid_lesson_formated_for_calendar });
+  },
 }));
