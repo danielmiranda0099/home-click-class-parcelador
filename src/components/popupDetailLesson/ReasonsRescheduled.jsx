3c3cbd4f065f8fda2fe897929cd9e3c2f81fc686
danielmiranda0099 <danielmesa877@gmail.com>
2024-11-15 11:28:37 -0500

    [ADD] Add component ReasonsRescheduled

---
+++ src/components/popupDetailLesson/ReasonsRescheduled.jsx
@@ -0,0 +1,25 @@
+export function ReasonsRescheduled({ reason = "" }) {
+  const parts = reason.split(/(\$%|\$&)/);
+  console.log(parts);
+  return (
+    <>
+      {parts.map((part, index) => {
+        if (part === "$%") {
+          return (
+            <p key={index} className="text-base font-medium mt-1">
+              {parts[index + 1]}
+            </p>
+          );
+        } else if (part === "$&") {
+          return (
+            <p key={index} className="text-sm mb-1 text-muted-foreground">
+              {parts[index + 1]}
+            </p>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </>
+  );
+}
