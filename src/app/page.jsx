c7b3db7790331906762e49604ac053aaca540da4
danielmiranda0099 <danielmesa877@gmail.com>
2024-11-15 12:00:57 -0500

    [UP] Up login, redirect if user is login

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,23 @@
 import Image from "next/image";
 import { FormLogin } from "@/components/auth";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+  const session = await auth();
+
+  if (session?.user) {
+    if (
+      session.user.role.includes("student") ||
+      session.user.role.includes("teacher")
+    ) {
+      redirect("/classes");
+    }
+    if (session.user.role.includes("admin")) {
+      redirect("/dashboard");
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="mx-auto w-[95%] max-w-sm md:w-[40%] md:max-w-md lg:w-[20%] space-y-6">
