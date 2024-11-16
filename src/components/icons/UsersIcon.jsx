a2d5acb294df07bd31fdc9b0bff1d7015300f237
danielmiranda0099 <danielmesa877@gmail.com>
2024-11-16 12:48:21 -0500

    [UP UI]

--- src/components/icons/UsersIcon.jsx
+++ src/components/icons/UsersIcon.jsx
@@ -1,16 +1,20 @@
-export function UsersIcon(props) {
+export function UsersIcon({
+  size = 24,
+  color = "currentColor",
+  className = "",
+}) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={size}
+      height={size}
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke={color}
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      className={className}
     >
       <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
       <circle cx="9" cy="7" r="4" />
