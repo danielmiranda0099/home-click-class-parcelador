6a87e84aad78c51242e9cacdc9f023f6c666c8b1
danielmiranda0099 <danielmesa877@gmail.com>
2024-11-20 12:20:49 -0500

    [UP] Add custom Props

--- src/components/icons/UserIcon.jsx
+++ src/components/icons/UserIcon.jsx
@@ -1,16 +1,20 @@
-export function UserIcon(props) {
+export function UserIcon({
+  size = 24,
+  color = "currentColor",
+  className = "",
+}) {
   return (
     <svg
-      {...props}
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={size}
+      height={size}
       viewBox="0 0 24 24"
       fill="none"
-      stroke="currentColor"
+      stroke={color}
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      className={className}
     >
       <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
       <circle cx="12" cy="7" r="4" />
