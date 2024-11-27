f0f17c6e8e17f84f17a254368609f5331154f41b
danielmiranda0099 <danielmesa877@gmail.com>
2024-11-27 15:38:02 -0500

    [FIX UP UI] Active Link Sidebar Menu

--- src/components/SideBarMenu.jsx
+++ src/components/SideBarMenu.jsx
@@ -20,52 +20,52 @@ export function SideBarMenu() {
           <nav className="space-y-2 p-4">
             <Link
               href="/admin/dashboard"
-              className={`flex items-center space-x-2 px-2 py-1 text-gray-600 hover:bg-gray-100 ${path_name === "/admin/dashboard" && "font-bold bg-blue-50 text-blue-500 border-b-4 border-blue-500"}`}
+              className={`flex items-center space-x-2 px-2 py-1 text-gray-600 hover:bg-gray-100 ${path_name.includes("/admin/dashboard") && "font-bold bg-blue-50 text-blue-500 border-b-4 border-blue-500"}`}
             >
               <DashboardIcon
-                color={`${path_name === "/admin/dashboard" ? "#3b82f6" : "currentColor"}`}
+                color={`${path_name.includes("/admin/dashboard") ? "#3b82f6" : "currentColor"}`}
               />
               <span
-                className={`${path_name === "/admin/dashboard" && "text-blue-500"}`}
+                className={`${path_name.includes("/admin/dashboard") && "text-blue-500"}`}
               >
                 Dashboard
               </span>
             </Link>
             <Link
               href="/admin/classes"
-              className={`flex items-center space-x-2 px-2 py-1 text-gray-600 hover:bg-gray-100 ${path_name === "/admin/classes" && "font-bold bg-blue-50 text-blue-500 border-b-4 border-blue-500"}`}
+              className={`flex items-center space-x-2 px-2 py-1 text-gray-600 hover:bg-gray-100 ${path_name.includes("/admin/classes") && "font-bold bg-blue-50 text-blue-500 border-b-4 border-blue-500"}`}
             >
               <BookOpenCheckIcon
-                color={`${path_name === "/admin/classes" ? "#3b82f6" : "currentColor"}`}
+                color={`${path_name.includes("/admin/classes") ? "#3b82f6" : "currentColor"}`}
               />
               <span
-                className={`${path_name === "/admin/classes" && "text-blue-500"}`}
+                className={`${path_name.includes("/admin/classes") && "text-blue-500"}`}
               >
                 Classes
               </span>
             </Link>
             <Link
               href="/admin/users"
-              className={`flex items-center space-x-2 px-2 py-1 text-gray-600 hover:bg-gray-100 ${path_name === "/admin/users" && "font-bold bg-blue-50 text-blue-500 border-b-4 border-blue-500"}`}
+              className={`flex items-center space-x-2 px-2 py-1 text-gray-600 hover:bg-gray-100 ${path_name.includes("/admin/users") && "font-bold bg-blue-50 text-blue-500 border-b-4 border-blue-500"}`}
             >
               <UsersIcon
-                color={`${path_name === "/admin/users" ? "#3b82f6" : "currentColor"}`}
+                color={`${path_name.includes("/admin/users") ? "#3b82f6" : "currentColor"}`}
               />
               <span
-                className={`${path_name === "/admin/users" && "text-blue-500"}`}
+                className={`${path_name.includes("/admin/users") && "text-blue-500"}`}
               >
                 Users
               </span>
             </Link>
             <Link
               href="/admin/accounting"
-              className={`flex items-center space-x-2 px-2 py-1 text-gray-600 hover:bg-gray-100 ${path_name === "/admin/accounting" && "font-bold bg-blue-50 text-blue-500 border-b-4 border-blue-500"}`}
+              className={`flex items-center space-x-2 px-2 py-1 text-gray-600 hover:bg-gray-100 ${path_name.includes("/admin/accounting") && "font-bold bg-blue-50 text-blue-500 border-b-4 border-blue-500"}`}
             >
               <DollarIcon
-                color={`${path_name === "/admin/accounting" ? "#3b82f6" : "currentColor"}`}
+                color={`${path_name.includes("/admin/accounting") ? "#3b82f6" : "currentColor"}`}
               />
               <span
-                className={`${path_name === "/admin/accounting" && "text-blue-500"}`}
+                className={`${path_name.includes("/admin/accounting") && "text-blue-500"}`}
               >
                 Accounting
               </span>
