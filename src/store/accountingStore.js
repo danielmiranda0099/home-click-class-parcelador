1eddd5414e881ddf8f378c2803e40a32aada8d38
danielmiranda0099 <danielmesa877@gmail.com>
2024-12-14 13:54:52 -0500

    [ADD] Edit Debt Store

--- src/store/accountingStore.js
+++ src/store/accountingStore.js
@@ -3,4 +3,6 @@ import { create } from "zustand";
 export const useAccountingStore = create((set) => ({
   editTransaction: null,
   setEditTransaction: (editTransaction) => set({ editTransaction }),
+  editDebt: null,
+  setEditDebt: (editDebt) => set({ editDebt }),
 }));
