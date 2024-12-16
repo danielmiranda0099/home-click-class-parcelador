166d599c4cc09bee7f836461e37fa1a52abe0637
danielmiranda0099 <danielmesa877@gmail.com>
2024-12-16 15:28:29 -0500

    [ADD] Filter Debt

--- src/components/accounting/TableDebt.jsx
+++ src/components/accounting/TableDebt.jsx
@@ -11,17 +11,25 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { PencilIcon, SearchIcon, TrashIcon } from "@/components/icons";
+import {
+  FilterIcon,
+  PencilIcon,
+  SearchIcon,
+  TrashIcon,
+} from "@/components/icons";
 import { formatCurrency } from "@/utils/formatCurrency";
 import { useLessonsStore } from "@/store/lessonStore";
 import { useUiStore } from "@/store/uiStores";
 import { PopupDeleteTransaction } from "./PopupDeleteTransaction";
 import { deleteDebts } from "@/actions/accounting";
 import { useAccountingStore } from "@/store/accountingStore";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Label } from "../ui/label";
 
 export function TableDebt({ debts, handleGetAllDebt, setIsOpenFormDebt }) {
   const [is_open_popup_delete, setIsOpenPopupDelete] = useState(false);
   const [transaction_id_current, setTransactionIdCurrent] = useState(null);
+  const [filter_debt, setFilterDebt] = useState("all");
   const { lessons, setSelectedLesson, setLessons } = useLessonsStore();
   const { setEditDebt } = useAccountingStore();
 
@@ -71,9 +79,42 @@ export function TableDebt({ debts, handleGetAllDebt, setIsOpenFormDebt }) {
         <CardHeader className="flex justify-between items-center">
           <CardTitle>Cartera</CardTitle>
         </CardHeader>
-        <CardContent className="flex flex-col justify-between h-[40rem] p-2 pb-6 gap-3">
+        <CardContent className="flex flex-col justify-between h-[45rem] p-2 pb-6 gap-3">
           {debts?.length > 0 ? (
-            <>
+            <div className="flex flex-col justify-start h-[31rem] p-2 pb-6 gap-3">
+              <div className="flex gap-2">
+                <FilterIcon />
+                <RadioGroup
+                  defaultValue="all"
+                  onValueChange={(value) => setFilterDebt(value)}
+                  className="flex gap-4"
+                >
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem
+                      value="all"
+                      id="r-all"
+                      className="data-[state=checked]:text-blue-400"
+                    />
+                    <Label htmlFor="r-all">Todo</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem
+                      value="income"
+                      id="r-income"
+                      className="data-[state=checked]:text-green-400"
+                    />
+                    <Label htmlFor="r-income">Ingreso</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem
+                      value="expense"
+                      id="r-expense"
+                      className="data-[state=checked]:text-red-400"
+                    />
+                    <Label htmlFor="r-expense">Egreso</Label>
+                  </div>
+                </RadioGroup>
+              </div>
               <Table className="relative">
                 <TableHeader className="bg-slate-900 sticky top-0">
                   <TableRow className="hover:bg-current">
@@ -82,122 +123,130 @@ export function TableDebt({ debts, handleGetAllDebt, setIsOpenFormDebt }) {
                     <TableHead className=""></TableHead>
                   </TableRow>
                 </TableHeader>
-                <TableBody className="border-gray-100 border-2 max-h-[40rem] overflow-y-scroll">
-                  {visibleDebts?.map((debt) => (
-                    <TableRow key={debt.id}>
-                      <TableCell
-                        className={`py-0 ${debt.type === "income" ? "text-green-600" : "text-red-600"}`}
-                      >
-                        {debt.type === "expense" && "-"}
-                        {formatCurrency(debt.amount)}
-                      </TableCell>
+                <TableBody className="border-gray-100 border-2 max-h-[31rem] overflow-y-scroll">
+                  {visibleDebts
+                    ?.filter(
+                      (debt) =>
+                        filter_debt === "all" || debt.type === filter_debt
+                    )
+                    .map((debt) => (
+                      <TableRow key={debt.id}>
+                        <TableCell
+                          className={`py-0 ${debt.type === "income" ? "text-green-600" : "text-red-600"}`}
+                        >
+                          {debt.type === "expense" && "-"}
+                          {formatCurrency(debt.amount)}
+                        </TableCell>
 
-                      <TableCell className="py-0">{debt.concept}</TableCell>
-                      <TableCell className="py-1">
-                        {debt.lessonId && (
-                          <Button
-                            variant="outline"
-                            size="icon"
-                            onClick={() => handleShowLesson(debt.lessonId)}
-                          >
-                            Ver
-                          </Button>
-                        )}
-                        {!debt.lessonId && (
-                          <>
+                        <TableCell className="py-0">{debt.concept}</TableCell>
+                        <TableCell className="py-1">
+                          {debt.lessonId && (
                             <Button
-                              variant="ghost"
-                              size="sm"
-                              aria-label="Editar movimiento"
-                              onClick={() => {
-                                setEditDebt({
-                                  id: debt.id,
-                                  amount: debt.amount,
-                                  type: debt.type,
-                                  concept: debt.concept,
-                                });
+                              variant="outline"
+                              size="icon"
+                              onClick={() => handleShowLesson(debt.lessonId)}
+                            >
+                              Ver
+                            </Button>
+                          )}
+                          {!debt.lessonId && (
+                            <>
+                              <Button
+                                variant="ghost"
+                                size="sm"
+                                aria-label="Editar movimiento"
+                                onClick={() => {
+                                  setEditDebt({
+                                    id: debt.id,
+                                    amount: debt.amount,
+                                    type: debt.type,
+                                    concept: debt.concept,
+                                  });
 
-                                setIsOpenFormDebt(true);
-                              }}
-                            >
-                              <PencilIcon className="h-4 w-4" />
-                            </Button>
-                            <Button
-                              variant="ghost"
-                              size="sm"
-                              aria-label="Eliminar movimiento"
-                              onClick={() => {
-                                setTransactionIdCurrent(debt.id);
-                                setIsOpenPopupDelete(true);
-                              }}
-                            >
-                              <TrashIcon className="h-4 w-4" />
-                            </Button>
-                          </>
-                        )}
-                      </TableCell>
-                    </TableRow>
-                  ))}
+                                  setIsOpenFormDebt(true);
+                                }}
+                              >
+                                <PencilIcon className="h-4 w-4" />
+                              </Button>
+                              <Button
+                                variant="ghost"
+                                size="sm"
+                                aria-label="Eliminar movimiento"
+                                onClick={() => {
+                                  setTransactionIdCurrent(debt.id);
+                                  setIsOpenPopupDelete(true);
+                                }}
+                              >
+                                <TrashIcon className="h-4 w-4" />
+                              </Button>
+                            </>
+                          )}
+                        </TableCell>
+                      </TableRow>
+                    ))}
                 </TableBody>
               </Table>
-              <div className="border-2 border-gray-100 bg-gray-50 p-3">
-                <div className="flex gap-2 justify-start pl-4 py-3 pb-1">
-                  <p className="font-medium">Total Ingresos:</p>
-                  <p className="font-bold text-green-400">
-                    {formatCurrency(total_income)}
-                  </p>
-                </div>
-
-                <div className="flex gap-2 justify-start pl-4 py-3 pt-0">
-                  <p className="font-medium">Total Egresos:</p>
-                  <p className="font-bold text-red-400">
-                    -{formatCurrency(total_expense)}
-                  </p>
-                </div>
-                <div className="flex gap-2 justify-start pl-4 py-3 pt-0">
-                  <p className="font-medium">Balance Cartera:</p>
-                  <p
-                    className={`font-bold ${total_balance >= 0 ? "text-blue-400" : "text-red-400"}`}
-                  >
-                    {formatCurrency(total_balance)}
-                  </p>
-                </div>
-
-                <div className="flex gap-3 justify-center items-center">
-                  <Button
-                    onClick={() =>
-                      setCurrentPage((prev) => Math.max(prev - 1, 0))
-                    }
-                    disabled={currentPage === 0}
-                  >
-                    Anterior
-                  </Button>
-                  <span>
-                    Página {currentPage + 1} de{" "}
-                    {Math.ceil(debts.length / itemsPerPage)}
-                  </span>
-                  <Button
-                    onClick={() =>
-                      setCurrentPage((prev) =>
-                        Math.min(
-                          prev + 1,
-                          Math.ceil(debts.length / itemsPerPage) - 1
-                        )
-                      )
-                    }
-                    disabled={endIndex >= debts.length}
-                  >
-                    Siguiente
-                  </Button>
-                </div>
-              </div>
-            </>
+            </div>
           ) : (
             <div className="flex flex-col gap-3 flex-1 justify-center items-center h-full">
               <SearchIcon size={"3rem"} />
               <p className="text-xl font-bold">
                 No se han encontrado movimientos.
               </p>
+            </div>
+          )}
+
+          {debts?.length > 0 && (
+            <div className="border-2 border-gray-100 bg-gray-50 p-3 ">
+              <div className="flex gap-2 justify-start pl-4 py-3 pb-1">
+                <p className="font-medium">Total Ingresos:</p>
+                <p className="font-bold text-green-400">
+                  {formatCurrency(total_income)}
+                </p>
+              </div>
+
+              <div className="flex gap-2 justify-start pl-4 py-3 pt-0">
+                <p className="font-medium">Total Egresos:</p>
+                <p className="font-bold text-red-400">
+                  -{formatCurrency(total_expense)}
+                </p>
+              </div>
+              <div className="flex gap-2 justify-start pl-4 py-3 pt-0">
+                <p className="font-medium">Balance Cartera:</p>
+                <p
+                  className={`font-bold ${total_balance >= 0 ? "text-blue-400" : "text-red-400"}`}
+                >
+                  {formatCurrency(total_balance)}
+                </p>
+              </div>
+
+              <div className="flex gap-3 justify-center items-center">
+                <Button
+                  onClick={() =>
+                    setCurrentPage((prev) => Math.max(prev - 1, 0))
+                  }
+                  disabled={currentPage === 0}
+                >
+                  Anterior
+                </Button>
+                <span>
+                  Página {currentPage + 1} de{" "}
+                  {Math.ceil(debts.length / itemsPerPage)}
+                </span>
+                <Button
+                  onClick={() =>
+                    setCurrentPage((prev) =>
+                      Math.min(
+                        prev + 1,
+                        Math.ceil(debts.length / itemsPerPage) - 1
+                      )
+                    )
+                  }
+                  disabled={endIndex >= debts.length}
+                >
+                  Siguiente
+                </Button>
+              </div>
             </div>
           )}
         </CardContent>
