47195fc4b1a2d9e682e68957f3384eeb28467a57
danielmiranda0099 <danielmesa877@gmail.com>
2024-12-19 11:07:15 -0500

    [ADD] HeaderAccountingHistory component with Form for annual and monthly balance retrieval and error handling

--- src/components/accountingHistory/HeaderAccountingHistory.jsx
+++ src/components/accountingHistory/HeaderAccountingHistory.jsx
@@ -1,8 +1,58 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useFormState, useFormStatus } from "react-dom";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Search } from "lucide-react";
+import { SearchIcon } from "@/components/icons";
+import { ErrorAlert } from "..";
+import { getAnnualAndMonthlyBalance } from "@/actions/accounting";
 
-export function HeaderAccountingHistory() {
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      type="submit"
+      disabled={pending}
+      className="flex gap-1 justify-center items-center"
+    >
+      <SearchIcon className="mr-2 h-4 w-4" />
+      Search
+    </Button>
+  );
+}
+
+export function HeaderAccountingHistory({ handleAction = null }) {
+  const [form_state_form_search_year, dispathFormSearchYear] = useFormState(
+    getAnnualAndMonthlyBalance,
+    {
+      data: [],
+      success: null,
+      error: false,
+      message: null,
+    }
+  );
+  const [error_message_form_search_year, setErrorMessageFormSearchYear] =
+    useState("");
+
+  const onGetAnnualAndMonthlyBalance = (form_data) => {
+    dispathFormSearchYear(form_data.get("year"));
+    setErrorMessageFormSearchYear("");
+  }
+
+  useEffect(() => {
+    if (form_state_form_search_year.success) {
+      if (handleAction) {
+        handleAction(form_state_form_search_year.data);
+      }
+    }
+    if (form_state_form_search_year.error) {
+      setErrorMessageFormSearchYear(form_state_form_search_year.message);
+    } else {
+      setErrorMessageFormSearchYear("");
+    }
+  }, [form_state_form_search_year]);
+
   return (
     <header className="flex flex-col items-between justify-center gap-3 mb-6">
       <h1 className="text-3xl font-bold">Contabilidad financiera</h1>
@@ -10,21 +60,23 @@ export function HeaderAccountingHistory() {
         Ingrese un año para comenzar y ver su historial de transacciones.
       </p>
 
-      <div className="w-full max-w-sm space-y-2">
+      <form className="w-full max-w-sm space-y-2" action={onGetAnnualAndMonthlyBalance}>
         <div className="flex space-x-2">
           <Input
-            id="year-input"
+            id="year"
+            name="year"
             type="number"
-            min="1900"
-            max="2100"
+            min="2000"
+            max="3000"
             className="w-32"
+            required
           />
-          <Button>
-            <Search className="mr-2 h-4 w-4" />
-            Search
-          </Button>
+          <SubmitButton />
         </div>
-      </div>
+        <div className="mb-6">
+          <ErrorAlert message={error_message_form_search_year} />
+        </div>
+      </form>
     </header>
   );
 }
