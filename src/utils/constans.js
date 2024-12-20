5b9d907d3424a8bbe0ff617e9052f1ca63c49516
danielmiranda0099 <danielmesa877@gmail.com>
2024-12-19 19:05:24 -0500

    [ADD] MONTHS_OF_YEAR constant for Spanish month names

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -23,3 +23,18 @@ export const DATA_LESSON_DEFAULT = {
   selectedDays: [],
   times: {},
 };
+
+export const MONTHS_OF_YEAR = {
+  1: "Enero",
+  2: "Febrero",
+  3: "Marzo",
+  4: "Abril",
+  5: "Mayo",
+  6: "Junio",
+  7: "Julio",
+  8: "Agosto",
+  9: "Septiembre",
+  10: "Octubre",
+  11: "Noviembre",
+  12: "Diciembre",
+};
