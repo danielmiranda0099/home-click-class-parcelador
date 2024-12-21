627143e86496ee2a30191fa3e3bd4cf17278c37b
danielmiranda0099 <danielmesa877@gmail.com>
2024-12-21 11:00:20 -0500

    [ADD UI] Add favicon links to RootLayout for improved branding

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,6 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+      <link rel="icon" href="/logohomeclick-32x32.png" sizes="32x32" />
+      <link rel="icon" href="/logohomeclick-180x180.png" sizes="192x192" />
+      </head>
       <body className={inter.className}>
         {children}
         <Toaster />
