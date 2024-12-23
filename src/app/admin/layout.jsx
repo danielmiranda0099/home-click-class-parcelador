2be290cb00bec212ae79b2f2fc39c61f8ec014c1
danielmiranda0099 <danielmesa877@gmail.com>
2024-12-23 18:06:50 -0500

    [UP REFACT] Update RootLayout to enforce admin role check and redirect unauthorized users

--- src/app/admin/layout.jsx
+++ src/app/admin/layout.jsx
@@ -1,15 +1,23 @@
+import { auth } from "@/auth";
 import { Header } from "@/components";
 import { FormRegisterUser } from "@/components/auth";
 import { PopupFormCreateNewLesson } from "@/components/PopupFormCreateNewLesson/PopupFormCreateNewLesson";
 
 import { SideBarMenu } from "@/components/SideBarMenu";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Gestion de horarios y contabilidad homeclikclass",
   description: "Gestion de horarios y contabilidad homeclikclass ",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const session = await auth();
+
+  if (!session || session?.user?.role[0] !== "admin") {
+    redirect("/api/auth/logout");
+  }
+
   return (
     <>
       <Header />
