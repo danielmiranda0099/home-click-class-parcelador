5a9b1c39b920bedda8742361767797e0aa332ca1
danielmiranda0099 <danielmesa877@gmail.com>
2024-12-23 18:07:01 -0500

    [UP REFACT] Enhance RootLayout to check user roles and redirect unauthorized access

--- src/app/(student-teacher)/layout.jsx
+++ src/app/(student-teacher)/layout.jsx
@@ -1,11 +1,19 @@
+import { auth } from "@/auth";
 import { Header } from "@/components";
+import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Gestion de horarios y contabilidad homeclikclass",
   description: "Gestion de horarios y contabilidad homeclikclass ",
 };
 
-export default function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
+  const session = await auth();
+
+  if (!session || (session?.user?.role[0] !== "student" && session?.user?.role[0] !== "teacher")) {
+    redirect("/api/auth/logout");
+  }
+
   return (
     <main>
       <Header />
