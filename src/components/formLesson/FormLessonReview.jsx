a1318b8523058153940f4c1b32ae2b37643605b8
danielmiranda0099 <danielmesa877@gmail.com>
2024-12-27 15:46:38 -0500

    [ADD] implement lesson score and student observations input fields in FormLessonReview

--- src/components/formLesson/FormLessonReview.jsx
+++ src/components/formLesson/FormLessonReview.jsx
@@ -1,3 +1,4 @@
+import { Input } from "../ui/input";
 import { Label } from "../ui/label";
 import { Textarea } from "../ui/textarea";
 
@@ -5,6 +6,48 @@ export function FormLessonReview({ lesson, rol }) {
   return (
     <>
       <div className="flex flex-wrap -mx-2 mb-4">
+        <div className="w-full mb-4">
+          {lesson?.isConfirmed &&
+            lesson.studentLessons.map((lesson_student) => (
+              <div className="grid grid-cols-3" key={lesson_student.id}>
+                {lesson.isGroup && lesson_student?.lessonScore && (
+                  <div className="flex items-center gap-4 w-full">
+                    {`${lesson_student.student.firstName.split(" ")[0]} ${lesson_student.student.lastName.split(" ")[0]}`}
+                  </div>
+                )}
+                {lesson_student?.lessonScore && (
+                  <div className="w-full px-2 mb-4 sm:mb-0">
+                    <Label htmlFor="lesson-score" className="block mb-1">
+                      Rating Class
+                    </Label>
+                    <Input
+                      type="number"
+                      id="lesson-score"
+                      name="lesson_score"
+                      min="1"
+                      max="10"
+                      defaultValue={
+                        (lesson_student?.lessonScore).toString() || ""
+                      }
+                    />
+                  </div>
+                )}
+
+                {lesson_student?.studentObservations && (
+                  <div className="w-full px-2 mb-4 sm:mb-0">
+                    <Label htmlFor="observations">Opinion</Label>
+                    <Textarea
+                      id="observations"
+                      name="student_observations"
+                      placeholder="Enter class Opinion"
+                      defaultValue={lesson_student?.studentObservations || ""}
+                    />
+                  </div>
+                )}
+              </div>
+            ))}
+        </div>
+
         <div className="w-full sm:w-1/5 px-2 mb-4 sm:mb-0">
           <Label htmlFor="oriented-week" className="block mb-1">
             Oriented Week
