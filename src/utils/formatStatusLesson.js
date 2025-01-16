e96b47f00888c7afd64038c8707852a790a8d474
danielmiranda0099 <danielmesa877@gmail.com>
2025-01-16 10:00:51 -0500

    [REFACT] Extract statusLesson function into a separate file for improved organization and maintainability

---
+++ src/utils/formatStatusLesson.js
@@ -0,0 +1,204 @@
+"use server";
+import { auth } from "@/auth";
+import { COLORS } from "./colorsStatusLesson";
+export async function statusLesson(lesson, rol) {
+  const session = await auth();
+  if (!lesson) {
+    return [COLORS.FUCHSIA_BG, COLORS.FUCHSIA_TEXT, "Unknown Status"];
+  }
+
+  if (lesson.isCanceled) {
+    return [COLORS.RED_BG, COLORS.RED_TEXT, "Canceled"]; //RED
+  }
+
+  if (rol === "student") {
+    if (
+      lesson?.isScheduled &&
+      lesson?.studentLessons.find(
+        (student_lesson) =>
+          student_lesson.studentId === parseInt(session?.user?.id, 10)
+      ).isConfirmed &&
+      !lesson?.studentLessons.find(
+        (student_lesson) =>
+          student_lesson.studentId === parseInt(session?.user?.id, 10)
+      ).isStudentPaid
+    ) {
+      return [
+        COLORS.YELLOW_BG,
+        COLORS.YELLOW_TEXT,
+        "Finalizada - Pendiente De Pago",
+      ]; //YELLOW
+    }
+    if (
+      lesson?.isScheduled &&
+      lesson?.studentLessons.find(
+        (student_lesson) =>
+          student_lesson.studentId === parseInt(session?.user?.id, 10)
+      ).isConfirmed &&
+      lesson?.studentLessons.find(
+        (student_lesson) =>
+          student_lesson.studentId === parseInt(session?.user?.id, 10)
+      ).isStudentPaid
+    ) {
+      return [COLORS.GREEN_BG, COLORS.GREEN_TEXT, "Finalizada - Pagada"]; //Green
+    }
+    if (
+      lesson?.isScheduled &&
+      lesson?.studentLessons.some((lesson) => lesson.isStudentPaid)
+    ) {
+      return [COLORS.BLUE_BG, COLORS.BLUE_TEXT, "Agendada - Pagada"]; //BLUE
+    }
+    if (
+      lesson?.isScheduled &&
+      !lesson?.studentLessons.some((lesson) => lesson.isStudentPaid)
+    ) {
+      return [COLORS.BLUE_BG, COLORS.BLUE_TEXT, "Agendada - Pendiente De Pago"]; //BLUE
+    }
+  }
+
+  if (rol === "teacher") {
+    if (
+      lesson?.isScheduled &&
+      lesson?.isConfirmed &&
+      lesson?.isRegistered &&
+      lesson?.isTeacherPaid
+    ) {
+      return [COLORS.GREEN_BG, COLORS.GREEN_TEXT, "Registrada - Pagada"]; //GREEN
+    }
+
+    if (
+      lesson?.isScheduled &&
+      lesson?.isConfirmed &&
+      lesson?.isRegistered &&
+      !lesson?.isTeacherPaid
+    ) {
+      return [
+        COLORS.ORANGE_BG,
+        COLORS.ORANGE_TEXT,
+        "Registrada - Pendiente De Pago",
+      ]; //ORANGE
+    }
+
+    if (lesson?.isScheduled && lesson?.isConfirmed) {
+      return [COLORS.PURPLE_BG, COLORS.PURPLE_TEXT, "Clase Confirmada"]; //PURPLE
+    }
+
+    if (lesson?.isScheduled) {
+      return [COLORS.BLUE_BG, COLORS.BLUE_TEXT, "Scheduled"]; //BLUE
+    }
+  }
+
+  if (rol === "admin") {
+    if (
+      lesson?.isScheduled &&
+      lesson?.isConfirmed &&
+      lesson?.isRegistered &&
+      !lesson?.isTeacherPaid &&
+      !lesson?.studentLessons.some((lesson) => lesson.isStudentPaid)
+    ) {
+      return [
+        COLORS.ORANGE_BG,
+        COLORS.ORANGE_TEXT,
+        lesson?.isGroup
+          ? "Registrada"
+          : "Registrada - Pendiente Pago Profesor - Pendiente Pago Estudiante",
+      ]; //ORANGE
+    }
+    if (
+      lesson?.isScheduled &&
+      lesson?.isConfirmed &&
+      lesson?.isRegistered &&
+      !lesson?.isTeacherPaid &&
+      lesson?.studentLessons.some((lesson) => lesson.isStudentPaid)
+    ) {
+      return [
+        COLORS.ORANGE_BG,
+        COLORS.ORANGE_TEXT,
+        lesson?.isGroup
+          ? "Registrada"
+          : "Registrada - Pendiente Pago Profesor - Pago Realizado Por Estudiante",
+      ]; //ORANGE
+    }
+    if (
+      lesson?.isScheduled &&
+      lesson?.isConfirmed &&
+      lesson?.isRegistered &&
+      lesson?.isTeacherPaid &&
+      !lesson?.studentLessons.some((lesson) => lesson.isStudentPaid)
+    ) {
+      return [
+        COLORS.YELLOW_BG,
+        COLORS.YELLOW_TEXT,
+        lesson?.isGroup
+          ? "Registrada"
+          : "Registrada - Pago Realizado Al Profesor - Pendiente Pago Estudiante",
+      ]; //YELLOW
+    }
+    if (
+      lesson?.isScheduled &&
+      lesson?.isConfirmed &&
+      lesson?.isRegistered &&
+      lesson?.isTeacherPaid &&
+      lesson?.studentLessons.some((lesson) => lesson.isStudentPaid)
+    ) {
+      return [
+        COLORS.GREEN_BG,
+        COLORS.GREEN_TEXT,
+        lesson?.isGroup
+          ? "Registrada"
+          : "Registrada - Pago Realizado Al Profesor - Pago Realizado Por Estudiante",
+      ]; //GREEN
+    }
+
+    if (
+      lesson?.isScheduled &&
+      lesson?.isConfirmed &&
+      !lesson?.studentLessons.some((lesson) => lesson.isStudentPaid)
+    ) {
+      return [
+        COLORS.PURPLE_BG,
+        COLORS.PURPLE_TEXT,
+        lesson?.isGroup
+          ? "Clase Confirmada"
+          : "Clase Confirmada - Pendiente Pago Estudiante",
+      ]; //PURPLE
+    }
+    if (
+      lesson?.isScheduled &&
+      lesson?.isConfirmed &&
+      lesson?.studentLessons.some((lesson) => lesson.isStudentPaid)
+    ) {
+      return [
+        COLORS.PURPLE_BG,
+        COLORS.PURPLE_TEXT,
+        lesson?.isGroup
+          ? "Clase Confirmada"
+          : "Clase Confirmada - Pago Realizado Por Estudiante",
+      ]; //PURPLE
+    }
+    if (
+      lesson?.isScheduled &&
+      !lesson?.studentLessons.some((lesson) => lesson.isStudentPaid)
+    ) {
+      return [
+        COLORS.BLUE_BG,
+        COLORS.BLUE_TEXT,
+        lesson?.isGroup ? "Agendada" : "Agendada - Pendiente Pago Estudiante",
+      ]; //BLUE
+    }
+    if (
+      lesson?.isScheduled &&
+      lesson?.studentLessons.some((lesson) => lesson.isStudentPaid)
+    ) {
+      return [
+        COLORS.BLUE_BG,
+        COLORS.BLUE_TEXT,
+        lesson?.isGroup
+          ? "Agendada"
+          : "Agendada - Pago Realizado Por Estudiante",
+      ]; //BLUE
+    }
+  }
+
+  return [COLORS.FUCHSIA_BG, COLORS.FUCHSIA_TEXT, "Unknown Status"];
+}
