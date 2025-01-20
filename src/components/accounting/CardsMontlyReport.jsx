a2b3fd28e34b6ad1524873e4cae214344b8aacba
danielmiranda0099 <danielmesa877@gmail.com>
2025-01-20 12:55:57 -0500

    [UP UI] Adjust layout and text size in CardsMontlyReport component for improved responsiveness

--- src/components/accounting/CardsMontlyReport.jsx
+++ src/components/accounting/CardsMontlyReport.jsx
@@ -4,7 +4,7 @@ import { formatCurrency } from "@/utils/formatCurrency";
 
 export function CardsMontlyReport({ monhtly_transactions }) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-3 mb-2">
+    <div className="grid grid-cols-2 md:grid-cols-3 gap-3 mb-2">
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">
@@ -13,7 +13,7 @@ export function CardsMontlyReport({ monhtly_transactions }) {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold text-green-400">
+          <div className="text-xl sm:text-2xl font-bold text-green-400">
             {monhtly_transactions
               ? formatCurrency(monhtly_transactions.total_income)
               : "$0"}
@@ -28,7 +28,7 @@ export function CardsMontlyReport({ monhtly_transactions }) {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold text-red-400">
+          <div className="text-xl sm:text-2xl font-bold text-red-400">
             {monhtly_transactions
               ? formatCurrency(monhtly_transactions.total_expense)
               : "$0"}
@@ -44,7 +44,7 @@ export function CardsMontlyReport({ monhtly_transactions }) {
         </CardHeader>
         <CardContent>
           <div
-            className={`text-2xl font-bold ${monhtly_transactions?.balance >= 0 ? "text-blue-400" : "text-red-400"}`}
+            className={`text-xl sm:text-2xl font-bold ${monhtly_transactions?.balance >= 0 ? "text-blue-400" : "text-red-400"}`}
           >
             {monhtly_transactions
               ? formatCurrency(monhtly_transactions.balance)
