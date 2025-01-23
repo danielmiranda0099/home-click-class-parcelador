30a23c484e85ff69abf99979c468749ceb11d08f
danielmiranda0099 <danielmesa877@gmail.com>
2025-01-23 07:44:19 -0500

    [REFACT] Simplify setLessons function in lesson store for improved data handling

--- src/store/lessonStore.js
+++ src/store/lessonStore.js
@@ -4,17 +4,15 @@ import { formattedLessonsForCalendar } from "@/utils/formattedLessonsForCalendar
 
 export const useLessonsStore = create((set) => ({
   lessons: [],
-  setLessons: async (rol, is_set_lessons_filtered = false) => {
-    try {
-      set({ isLoading: true, error: null });
-      const data = await getLessons();
-      const formatted_lessons = await formattedLessonsForCalendar(data, rol);
-      set({ lessons: formatted_lessons, isLoading: false });
-      if (is_set_lessons_filtered) set({ lessons_filtered: formatted_lessons });
-      return formatted_lessons;
-    } catch (error) {
-      set({ error: error.message, isLoading: false });
-      throw error;
+  setLessons: async (rol, date_range) => {
+    const data = await getLessons(date_range);
+    if (data?.success) {
+      const formatted_lessons = await formattedLessonsForCalendar(
+        data.data,
+        rol
+      );
+      set({ lessons: formatted_lessons });
+      set({ lessons_filtered: formatted_lessons });
     }
   },
 
