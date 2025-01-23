d3c2f5afa18bc494edaf6b929c660201795e965e
danielmiranda0099 <danielmesa877@gmail.com>
2025-01-23 10:51:47 -0500

    [UP REFACT] Enhance setLessons function to cache loaded months and prevent redundant data fetching

--- src/store/lessonStore.js
+++ src/store/lessonStore.js
@@ -1,20 +1,32 @@
 import { create } from "zustand";
 import { getLessons } from "@/actions/CrudLesson";
 import { formattedLessonsForCalendar } from "@/utils/formattedLessonsForCalendar";
+import { getMonth, getYear } from "date-fns";
 
-export const useLessonsStore = create((set) => ({
+export const useLessonsStore = create((set, get) => ({
   lessons: [],
   setLessons: async (rol, date_range) => {
-    const data = await getLessons(date_range);
-    if (data?.success) {
-      const formatted_lessons = await formattedLessonsForCalendar(
-        data.data,
-        rol
-      );
-      set({ lessons: formatted_lessons });
-      set({ lessons_filtered: formatted_lessons });
+    const year = getYear(date_range.startOfMonth);
+    const month = getMonth(date_range.startOfMonth);
+    const key = `${year}-${month}`; // Crear una clave única basada en el año y el mes
+    const { loadedMonths } = get();
+    if (!loadedMonths.has(key)) {
+      loadedMonths.set(key, true); // Puedes guardar más datos si necesitas
+      set({ loadedMonths: new Map(loadedMonths) }); // Actualizar la store
+      const data = await getLessons(date_range);
+      if (data?.success) {
+        const formatted_lessons = await formattedLessonsForCalendar(
+          data.data,
+          rol
+        );
+        const { lessons } = get();
+        set({ lessons: [...lessons, ...formatted_lessons] });
+        set({ lessons_filtered: [...lessons, ...formatted_lessons] });
+      }
     }
+    console.log(loadedMonths);
   },
+  loadedMonths: new Map(),
 
   lessons_filtered: [],
   setLessonsFiltered: (new_lessons) =>
