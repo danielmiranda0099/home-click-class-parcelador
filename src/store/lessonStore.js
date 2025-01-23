303d023b56ae3e1b3692b30c50446dcb129c95ae
danielmiranda0099 <danielmesa877@gmail.com>
2025-01-23 15:18:41 -0500

    [UP REFACT] Modify setLessons function to support forced updates and improve loading state management

--- src/store/lessonStore.js
+++ src/store/lessonStore.js
@@ -5,24 +5,29 @@ import { getMonth, getYear } from "date-fns";
 
 export const useLessonsStore = create((set, get) => ({
   lessons: [],
-  setLessons: async (rol, date_range) => {
+  setLessons: async (date_range, forceUpdate = false) => {
     const year = getYear(date_range.startOfMonth);
     const month = getMonth(date_range.startOfMonth);
     const key = `${year}-${month}`; // Crear una clave única basada en el año y el mes
-    const { loadedMonths } = get();
-    if (!loadedMonths.has(key)) {
+    if (forceUpdate) {
+      set({ loadedMonths: new Map() });
+    }
+    let { loadedMonths } = get();
+    if (!loadedMonths.has(key) || forceUpdate) {
       loadedMonths.set(key, true); // Puedes guardar más datos si necesitas
       set({ loadedMonths: new Map(loadedMonths) }); // Actualizar la store
       set({ isLoadingLessons: true });
       const data = await getLessons(date_range);
       if (data?.success) {
-        const formatted_lessons = await formattedLessonsForCalendar(
-          data.data,
-          rol
-        );
+        const formatted_lessons = await formattedLessonsForCalendar(data.data);
         const { lessons } = get();
-        set({ lessons: [...lessons, ...formatted_lessons] });
-        set({ lessons_filtered: [...lessons, ...formatted_lessons] });
+        if (!forceUpdate) {
+          set({ lessons: [...lessons, ...formatted_lessons] });
+          set({ lessons_filtered: [...lessons, ...formatted_lessons] });
+        } else {
+          set({ lessons: formatted_lessons });
+          set({ lessons_filtered: formatted_lessons });
+        }
       }
       set({ isLoadingLessons: false });
     }
