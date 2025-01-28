9d44bb6a1f21985615a3fe735b74c4029491d3f3
danielmiranda0099 <danielmesa877@gmail.com>
2025-01-28 10:39:59 -0500

    [DOC] Add JSDoc comments to scheduleLessons function for better documentation

--- src/utils/scheduleLessons.js
+++ src/utils/scheduleLessons.js
@@ -1,6 +1,15 @@
 import moment from "moment";
 import { DAYS_OF_WEEK_NUMBER } from "./constans";
 
+/**
+ * Generates an array of scheduled lesson dates based on selected days, times, and a specific period.
+ *
+ * @param {string[]} selected_days - Selected days represented by their initials in Spanish. Possible values: ["LU", "MA", "MI", "JU", "VI", "SA", "DO"].
+ * @param {Object.<number, string>} times - Object that defines the time for each day of the week. The key is a number from 0 (Sunday) to 6 (Saturday), and the value is the time in "HH:mm" format.
+ * @param {string} period - Time period for generating dates. Possible values: "3M" (3 months), "6M" (6 months), "1Y" (1 year).
+ * @param {string} startDate - Start date in "YYYY-MM-DD" format.
+ * @returns {string[]} Array of scheduled lesson dates in ISO format.
+ */
 export function scheduleLessons(selected_days, times, period, startDate) {
   const dates = [];
   let currentDate = moment(startDate); // Iniciar desde la fecha proporcionada
