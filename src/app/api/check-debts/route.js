1806cb62881f255bb3eb8722725c31e43462dff8
danielmiranda0099 <danielmesa877@gmail.com>
2025-07-17 17:12:36 -0500

    [UP BACK] Weekly check debts

--- src/app/api/check-debts/route.js
+++ src/app/api/check-debts/route.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { Resend } from "resend";
 import { subWeeks } from "date-fns";
+import { PendingPaymentEmail } from "@/emails";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -10,7 +11,6 @@ export async function GET(request) {
   const secret = searchParams.get("secret");
 
   if (secret !== process.env.CRON_SECRET_KEY) {
-    console.log("****************************** no autorizado");
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   }
 
@@ -29,8 +29,8 @@ export async function GET(request) {
     },
     select: {
       id: true,
-      firstName: true,
-      email: true,
+      shortName: true,
+      personalEmail: true,
       studentLessons: {
         where: {
           isStudentPaid: false,
@@ -69,27 +69,25 @@ export async function GET(request) {
   }
 
   const debtors = Array.from(notifiedStudentsMap.values());
-
+  const problemsStudnets = [];
   for (const student of debtors) {
-    const reason =
-      student.unpaidCount > 4
-        ? "más de 4 clases sin pagar"
-        : "una clase vencida hace más de 3 semanas";
-
-    console.log(
-      `************ Hola ${student.firstName}, tienes ${student.unpaidCount} clases sin pagar (${reason}). Por favor ponte al día.`
-    );
-
-    // Descomenta para enviar email:
-    // await resend.emails.send({
-    //   from: "Academia <notificaciones@tudominio.com>",
-    //   to: student.email,
-    //   subject: "Tienes clases en deuda",
-    //   html: `<p>Hola ${student.firstName}, tienes ${student.unpaidCount} clases sin pagar (${reason}). Por favor ponte al día.</p>`,
-    // });
+    if (!student.personalEmail) {
+      problemsStudnets.push(student.shortName);
+      continue;
+    }
+    const { error } = await resend.emails.send({
+      from: "HOME CLICK CLASS <onboarding@resend.dev>",
+      to: student.personalEmail,
+      subject: "HOME CLICK CLASS ALERTS",
+      react: PendingPaymentEmail({ studentName: student.shortName }),
+    });
+    if (error) {
+      problemsStudnets.push(student.shortName);
+    }
   }
 
   return NextResponse.json({
     message: `Se notificaron ${debtors.length} estudiantes.`,
+    data: data,
   });
 }
