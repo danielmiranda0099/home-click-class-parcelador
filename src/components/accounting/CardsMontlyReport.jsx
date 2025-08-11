5c4a4417947df1761a22c30b6d6f7250d78df13d
danielmiranda0099 <danielmesa877@gmail.com>
2025-08-11 12:35:56 -0500

    [MINOR REMOV] unused MONTHS_OF_YEAR import in CardsMontlyReport component

--- src/components/accounting/CardsMontlyReport.jsx
+++ src/components/accounting/CardsMontlyReport.jsx
@@ -1,5 +1,4 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { MONTHS_OF_YEAR } from "@/utils/constans";
 import { formatCurrency } from "@/utils/formatCurrency";
 
 export function CardsMontlyReport({ monhtly_transactions }) {
