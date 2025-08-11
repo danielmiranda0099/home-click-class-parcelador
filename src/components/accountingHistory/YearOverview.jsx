ff99857a7074392224c9d7882739e50ba5cfeb37
danielmiranda0099 <danielmesa877@gmail.com>
2025-08-11 15:35:07 -0500

    [REFACT UP] Update YearOverview component layout and improve expense categorization

--- src/components/accountingHistory/YearOverview.jsx
+++ src/components/accountingHistory/YearOverview.jsx
@@ -1,12 +1,14 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { formatCurrency } from "@/utils/formatCurrency";
 
-export function YearOverview({year, balance}) {
+export function YearOverview({ year, balance }) {
   return (
-    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-2 lg:grid-cols-5 gap-4">
       <Card>
         <CardHeader className="pb-2">
-          <CardTitle className="text-sm font-medium">Total Ingresos {year}</CardTitle>
+          <CardTitle className="text-sm font-medium">
+            Total Ingresos {year}
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <div className={`text-lg lg:text-2xl font-bold text-green-400`}>
@@ -16,21 +18,51 @@ export function YearOverview({year, balance}) {
       </Card>
       <Card>
         <CardHeader className="pb-2">
-          <CardTitle className="text-sm font-medium">Total Egresos {year}</CardTitle>
+          <CardTitle className="text-sm font-medium">
+            Egresos Profesor {year}
+          </CardTitle>
         </CardHeader>
         <CardContent>
           <div className={`text-lg lg:text-2xl font-bold text-red-400`}>
-            -{formatCurrency(balance.expense)}
+            -{formatCurrency(balance.expenseTeacher)}
           </div>
         </CardContent>
       </Card>
       <Card>
         <CardHeader className="pb-2">
-          <CardTitle className="text-sm font-medium">Total Balance {year}</CardTitle>
+          <CardTitle className="text-sm font-medium">
+            Otros Egresos {year}
+          </CardTitle>
         </CardHeader>
         <CardContent>
-          <div className={`text-lg lg:text-2xl font-bold  ${(balance.income - balance.expense) >= 0 ? "text-blue-400" : "text-red-400"}`}>
-            {formatCurrency(balance.income - balance.expense)}
+          <div className={`text-lg lg:text-2xl font-bold text-red-400`}>
+            -{formatCurrency(balance.expenseOther)}
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="pb-2">
+          <CardTitle className="text-sm font-medium">
+            Total Egreso {year}
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className={`text-lg lg:text-2xl font-bold  text-red-400`}>
+            {formatCurrency(balance.totalExpense)}
+          </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader className="pb-2">
+          <CardTitle className="text-sm font-medium">
+            Total Balance {year}
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div
+            className={`text-lg lg:text-2xl font-bold  ${balance.balance >= 0 ? "text-blue-400" : "text-red-400"}`}
+          >
+            {formatCurrency(balance.balance)}
           </div>
         </CardContent>
       </Card>
